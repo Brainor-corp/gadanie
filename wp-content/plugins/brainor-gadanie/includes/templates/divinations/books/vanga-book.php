8fff6a5b3be2a13a6b2784308e264aad9fad1cce
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-02-14 09:32:42 +0400

    Добавляем анимированную воду для гадания "Гадание Ванги"

--- wp-content/plugins/brainor-gadanie/includes/templates/divinations/books/vanga-book.php
+++ wp-content/plugins/brainor-gadanie/includes/templates/divinations/books/vanga-book.php
@@ -1,30 +1,29 @@
 <script>
-    if (!document.getElementById('divination-magic-book-css')) {
+    if (!document.getElementById('divination-vanga-css')) {
         let style = document.createElement('link');
-        style.setAttribute('id', 'divination-magic-book-css');
+        style.setAttribute('id', 'divination-vanga-css');
         style.setAttribute('rel', 'stylesheet');
         style.setAttribute('type', 'text/css');
-        style.setAttribute('href', '/wp-content/plugins/brainor-gadanie/assets/css/divination-magic-book.css');
+        style.setAttribute('href', '/wp-content/plugins/brainor-gadanie/assets/css/divination-vanga.css');
 
         document.getElementsByTagName('head')[0].appendChild(style);
     }
 
-    if (!document.getElementById('divination-magic-book-js')) {
+    if (!document.getElementById('divination-vanga-js')) {
         let script = document.createElement('script');
-        script.src = '/wp-content/plugins/brainor-gadanie/assets/js/divination-magic-book.js';
-        script.id = 'divination-magic-book-js';
+        script.src = '/wp-content/plugins/brainor-gadanie/assets/js/divination-vanga.js';
+        script.id = 'divination-vanga-js';
         script.type = 'text/javascript';
         document.getElementsByTagName('head')[0].appendChild(script);
     }
 </script>
 
-<link rel="stylesheet" href="/wp-content/plugins/brainor-gadanie/assets/css/divination-magic-book-vanga.css">
-
-<div class="divination-magic-book vanga-book" id="<?php echo uniqid() ?>">
+<div class="divination-vanga" id="<?php echo uniqid() ?>">
     <div class="br-mb-questions">
 
         <!-- Блок с ответами -->
         <div class="br-mb-question-row">
+
             <div class="br-mb-answers">
 
                 <div class="br-mb-answer">
@@ -44,7 +43,9 @@
                 </div>
 
             </div>
-            <div class="br-mb-question">
+
+            <div class="br-mb-cup">
+                <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/cupq.gif" class="br-mb-cup-a" alt="">
             </div>
         </div>
         <!-- Конец блока с ответами -->
