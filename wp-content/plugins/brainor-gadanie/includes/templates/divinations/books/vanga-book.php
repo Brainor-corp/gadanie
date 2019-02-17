811b31ee588435f7c7e4478995f9c2c37269289b
Антон Выходцев <vykhodtsevai@gmail.com>
2019-02-18 02:47:07 +0300

    Вывод гаданий

--- wp-content/plugins/brainor-gadanie/includes/templates/divinations/books/vanga-book.php
+++ wp-content/plugins/brainor-gadanie/includes/templates/divinations/books/vanga-book.php
@@ -28,21 +28,135 @@
 
                 <div class="br-mb-answer">
                     <div>
-                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/van1.png" alt="">
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/gnezdo.png" alt="">
                         <br>
-                        <span>Он полюбил тебя с первого взгляда</span>
+                        <span>Гнездо. Все в вашей жизни повернется к лучшему.</span>
                     </div>
                 </div>
 
                 <div class="br-mb-answer">
                     <div>
-                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/van1.png" alt="">
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/lastochka.png" alt="">
                         <br>
-                        <span>Он полюбил тебя с первого взгляда 2</span>
+                        <span>Ласточка. Вас ждет встреча с человеком, с которым вы захотите связать свою жизнь.</span>
                     </div>
                 </div>
 
-            </div>
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/serdce.png" alt="">
+                        <br>
+                        <span>Сердце. Вас ждут новые отношения или новый виток в уже существующих.
+                        </span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/moneti.png" alt="">
+                        <br>
+                        <span>Монеты. Вас ждет богатство и благополучие.</span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/braslet.png" alt="">
+                        <br>
+                        <span>Браслет. Вас ждет неожиданность, которая окажется для вас очень приятной.</span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/nozhnici.png" alt="">
+                        <br>
+                        <span>Ножницы. Вы будете действовать решительно, и сможете очень добиться поставленной цели.
+                        </span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/mayak.png" alt="">
+                        <br>
+                        <span>Маяк. Вдохновение и прилив чувств помогут вам достичь вашей заветной мечты.</span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/angel.png" alt="">
+                        <br>
+                        <span>Ангел. Хороший знак. Вас ждут спокойные и очень счастливые времена.</span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/zvezda.png" alt="">
+                        <br>
+                        <span>Звезда. Ваша заветная мечта в скором будущем исполнится.</span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/kluch.png" alt="">
+                        <br>
+                        <span>Ключ. Вам откроются новые возможности в жизни.</span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/verevka.png" alt="">
+                        <br>
+                        <span>Веревка. Вас ждет событие, после которого вы сможете осуществить задуманное.
+                        </span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/vesi.png" alt="">
+                        <br>
+                        <span>Весы. Справедливость восторжествует, даже если вы не приложите к этому никаких усилий.
+                        </span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/svecha.png" alt="">
+                        <br>
+                        <span>Свеча. Ваши надежды оправдаются и все будет лучше и выгоднее для вас, чем вы думали.</span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/roza.png" alt="">
+                        <br>
+                        <span>Роза. В скором времени вы окажетесь во власти такого прекрасного чувства, как любовь.</span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/yablonia.png" alt="">
+                        <br>
+                        <span>Яблоня. Вас ожидает радостное событие, на душе будет светло и безмятежно.</span>
+                    </div>
+                </div>
+
+                <div class="br-mb-answer">
+                    <div>
+                        <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/monastir.png" alt="">
+                        <br>
+                        <span>Монастырь. Ваша жизнь станет насыщенной и переполненной событиями.</span>
+                    </div>
+                </div>
 
             <div class="br-mb-cup">
                 <img src="/wp-content/plugins/brainor-gadanie/assets/imgs/vanga/cupq.gif" class="br-mb-cup-a" alt="">
